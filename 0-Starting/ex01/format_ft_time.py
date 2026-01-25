a1baa292c5afb388f0d18106bdf6c494e2e7469d
Jaqezita <jaque_hp@hotmail.com>
2026-01-25 11:35:49 -0300

    feat: add ex01

---
+++ 0-Starting/ex01/format_ft_time.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+# Data inicial "Era Unix" (Unix Epoch)
+data_inicial = datetime(1970, 1, 1)
+
+# Data atual
+data_atual = datetime.now()
+
+# Diferença entre as duas datas
+diferenca = data_atual - data_inicial
+
+# Total de segundos desde a Era Unix
+total_segundos = diferenca.total_seconds()
+
+print(
+    f"Seconds since January 1, 1970: {total_segundos:,.3f} "
+    f"or {total_segundos:.2e} in scientific notation.")
+
+
+
+
+
+
+
+# import time
+
+# #Tempo atual em segundos desde a Era Unix
+# tempo_atual = time.time()
